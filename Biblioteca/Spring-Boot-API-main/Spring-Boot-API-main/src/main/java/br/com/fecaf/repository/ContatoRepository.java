dc4b9dcf5a24e18a63ceca2c9b97ab4d6891d908
Mikael Icaro <mikael.ikaro9@gmail.com>
2025-04-30 23:02:53 -0300

    Delete Biblioteca directory

--- Biblioteca/Spring-Boot-API-main/Spring-Boot-API-main/src/main/java/br/com/fecaf/repository/ContatoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fecaf.repository;
-
-import br.com.fecaf.model.Contato;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContatoRepository  extends JpaRepository<Contato, Integer> {
-}
